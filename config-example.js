0755f296d81113c8737e327058ee6236c44eb835
ricki gozal <37609653+ketekdude@users.noreply.github.com>
2021-08-05 18:06:00 +0700

    fix config.

--- config-example.js
+++ config-example.js
@@ -1,7 +1,9 @@
 
     var config = {};
     config.currentPage = '';
-    config.baseUri     = 'http://localhost/test-web/';
+    config.baseUri     = 'http://localhost/AdminLTE-3.1.0/';
     config.webProtocol = 'http://';
+    config.serviceUri = 'http://localhost/test-app/public/web/v1/';
+    // config.serviceUri = 'http://192.168.1.6/test-app/public/web/v1/';
     
     
